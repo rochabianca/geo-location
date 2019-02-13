e3df2000e6742f60dc153926d0ff2c40b8d0c605
Bianca <bianca.rocha1711@gmail.com>
2019-02-13 08:55:36 -0300

    start on the GMap component

---
+++ src/components/home/GMap.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="map">
+    <h2>Map</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GMap",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
+
--- src/router.js
+++ src/router.js
@@ -1,12 +1,17 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
+import GMap from '@/components/home/GMap';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-  
+    {
+      path: '/',
+      name: 'GMap',
+      component: GMap
+    }
   ]
-})
+});
