1160b613c17e70ae3a0962b0bdfaa8211bf09d5d
Bianca <bianca.rocha1711@gmail.com>
2019-02-14 09:11:33 -0300

    making the map show with the google maps api

--- src/components/home/GMap.vue
+++ src/components/home/GMap.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="map">
-    <h2>Map</h2>
+    <div
+      class="google-map"
+      id="map"
+    ></div>
   </div>
 </template>
 
 <script>
 export default {
   name: "GMap",
-  data() {
-    return {};
+  data () {
+    return {
+      lat: 53,
+      lng: -2,
+    };
+  },
+  methods: {
+    renderMap () {
+      const map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: this.lat, lng: this.lng },
+        zoom: 7,
+        maxZoom: 15,
+        minZoom: 3,
+        streetViewControl: false
+      })
+    }
+  },
+  mounted () {
+    this.renderMap()
   }
 };
 </script>
 
 <style lang="scss">
+.google-map {
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  background: white;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
 </style>
 
