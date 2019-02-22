04e1a40cb9eb9b932e1d81b5db1167fe41c4d866
Bianca <bianca.rocha1711@gmail.com>
2019-02-22 09:49:25 -0300

    protecting the GMap route from unlogged people

--- src/router.js
+++ src/router.js
@@ -1,19 +1,24 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import firebase from 'firebase';
+
 import GMap from '@/components/home/GMap';
 import Signup from '@/components/auth/Signup';
 import Login from '@/components/auth/Login';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'GMap',
-      component: GMap
+      component: GMap,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/signup',
@@ -27,3 +32,22 @@ export default new Router({
     }
   ]
 });
+
+router.beforeEach((to, from, next) => {
+  // check to see if route require auth
+  if (to.matched.some(rec => rec.meta.requiresAuth)) {
+    // check auth state of user
+    let user = firebase.auth().currentUser;
+    if (user) {
+      // user is signed in, procced to route
+      next();
+    } else {
+      // no user signed in, redirect to login
+      next({ name: 'Login' });
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
