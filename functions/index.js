0303f64007df4101c5b25c19d320bee4ef436ef5
Bianca <bianca.rocha1711@gmail.com>
2019-02-27 09:00:40 -0300

    start on firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,19 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.checkAlias = functions.https.onCall((data, context) => {
+  const ref = admin
+    .firestore()
+    .collection('users')
+    .doc(data.slug);
+  return ref
+    .get()
+    .then(doc => {
+      return { unique: !doc.exists };
+    })
+    .catch(err => {
+      throw new functions.https.HttpsError('failed to connect');
+    });
+});
